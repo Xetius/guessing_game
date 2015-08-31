dd35b2127e58afe0a87fc6b6a3562f852d085241
Chris Hudson <chris@xetius.com>
2015-08-31 13:10:30 +0100

    Added number validation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std:io;
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -10,5 +18,15 @@ fn main() {
         .ok()
         .expect("Failed to read line");
 
-    println!("Hello, world!");
+    let guess: u32 = guess.trim().parse()
+        .ok()
+        .expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
